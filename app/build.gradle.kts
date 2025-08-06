5a1eec22e2788c8c7c0863abbc938f044e53f434
KamaTAEWOO <zszs2020@naver.com>
2025-08-06 23:35:47 +0900

    [chore] 테스트 케이스 작성법

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // build.gradle
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockito.core)
 }
